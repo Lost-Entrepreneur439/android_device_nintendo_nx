4e6c798f9c5318bd656743f3c4921db86de31c77
Evelyn Torres <lostentrepreneur439@yahoo.com>
2026-07-21 16:50:22 -0230

    some more updates for AICP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ TARGET_COPY_OUT_ODM                    := odm
 TARGET_COPY_OUT_PRODUCT                := product
 TARGET_COPY_OUT_VENDOR                 := vendor
 
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/aicp/config/BoardConfigReservedSize.mk
 
 BOARD_NVIDIA_DYNAMIC_PARTITIONS_PARTITION_LIST := product system system_ext vendor odm
 BOARD_NVIDIA_DYNAMIC_PARTITIONS_SIZE           := 6169821184
